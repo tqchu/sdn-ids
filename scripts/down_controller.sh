2064779e1a4fb57d5bd1a31c72c93ef00e3951b4
chu.tq <chu.tq@teko.vn>
2025-05-28 21:38:50 +0700

    add scripts

---
+++ scripts/down_controller.sh
@@ -0,0 +1,20 @@
+# vmrun stop '/home/truongchu/VirtualMachines/Metasploitable2Linux/Metasploitable.vmx'
+# vmrun stop '/home/truongchu/.vagrant.d/boxes/rapid7-VAGRANTSLASH-metasploitable3-ub1404/0.1.12-weekly/vmware_desktop/metasploitable3-ub1404.vmx'
+# vmrun stop '/home/truongchu/.vagrant.d/boxes/rapid7-VAGRANTSLASH-metasploitable3-win2k8/0.1.0-weekly/vmware_desktop/metasploitable3-win2k8.vmx'
+
+sudo ovs-vsctl del-br br0
+sudo ip addr flush dev br0
+
+sudo ip addr add 198.51.100.1/24 dev ens33
+# sudo ip addr add 10.50.50.1/24 dev vmnet3
+
+sudo ip link set ens33 up
+# sudo ip link set vmnet3 up
+
+# sudo pkill snort
+docker stop goflow2
+docker rm goflow2
+
+docker stop ml_detector
+docker rm ml_detector
+#ps aux | grep snort
--- scripts/up_controller.sh
+++ scripts/up_controller.sh
@@ -1,22 +1,21 @@
 set -euo pipefail
 
-sshpass -p 'msfadmin' \
-  ssh -o StrictHostKeyChecking=no \
-      msfadmin@198.51.100.128 \
-      'echo msfadmin | sudo -S ip route add default via 198.51.100.1'
-
+sshpass -p 'msfadmin' ssh -o StrictHostKeyChecking=no \
+  msfadmin@198.51.100.128 \
+  "echo msfadmin | sudo -S ip route replace default via 198.51.100.130"
+echo "Configured default route ok"
 # sshpass -p 'vagrant' \
 #   ssh -o StrictHostKeyChecking=no \
 #       vagrant@10.50.50.128 \
 #       'echo vagrant | sudo -S ip route add default via 10.50.50.1'
 
 sudo ovs-vsctl add-br br0
-sudo ovs-vsctl add-port br0 vmnet2
-sudo ovs-vsctl add-port br0 vmnet3
-sudo ip addr flush dev vmnet2
-sudo ip addr flush dev vmnet3
+sudo ovs-vsctl add-port br0 ens33
+#sudo ovs-vsctl add-port br0 vmnet3
+# sudo ip addr flush dev vmnet2
+sudo ip addr flush dev ens33
 
-sudo ip addr add 10.50.50.1/24 dev br0
+# sudo ip addr add 10.50.50.1/24 dev br0
 sudo ip addr add 198.51.100.1/24 dev br0
 
 sudo ip link set br0 up
@@ -46,13 +45,13 @@ if $SNORT; then
   echo "[*] Snort is up."
 fi
 
-goflow2_container_id=$(docker run -d --name goflow2 --network controller_default -p 6343:6343/udp goflow2)
+goflow2_container_id=$(docker run -d --name goflow2 --network sdn-ids_default -p 6343:6343/udp truongquangchu/goflow2)
 echo "Goflow2 container ID: $goflow2_container_id"
 
-consumer_container_id=$(docker run -d --name ml_detector   --network controller_default   -v $(pwd)/flows:/app/flows   -e KAFKA_BOOTSTRAP_SERVERS=kafka:29092   -e PUSHGATEWAY_URL=pushgateway:9091   -e LOKI_URL=http://loki:3100 -e CONTROLLER_HOST=172.17.0.1  ml-detector)
+consumer_container_id=$(docker run -d --name ml_detector   --network sdn-ids_default   -v $(pwd)/flows:/app/flows   -e KAFKA_BOOTSTRAP_SERVERS=kafka:29092   -e PUSHGATEWAY_URL=pushgateway:9091   -e LOKI_URL=http://loki:3100 -e CONTROLLER_HOST=172.17.0.1  truongquangchu/ml-detector)
 echo "ML Detector container ID: $consumer_container_id"
 
-sudo ovs-vsctl -- --id=@sflow create sflow agent=wlp0s20f3     target=\"localhost:6343\" sampling=4 polling=10     -- set bridge br0 sflow=@sflow
+sudo ovs-vsctl -- --id=@sflow create sflow agent=ens33     target=\"localhost:6343\" sampling=4 polling=10     -- set bridge br0 sflow=@sflow
 export PYTHONPATH=$(pwd):$PYTHONPATH
 #ryu-manager controller.traffic_logger
 ryu-manager  --wsapi-port 8082 controller.flow_controller ryu.app.ofctl_rest
